f1ecf057e384a1a632afb5f7798832362c571fe8
Johannes J. Schmidt <schmidt@netzmerk.com>
2014-05-06 14:19:02 +0200

    validate config

--- index.js
+++ index.js
@@ -5,61 +5,71 @@
 var pkg = require('./package.json');
 var stream = require('./lib/listen');
 
+var assert = require('assert');
 var url = require('url');
 var async = require('async');
 var nano = require('nano');
 
+
+// used in client and test
+var defaultLogger = {
+  info: function(msg) {
+    console.info('[' + new Date + '] [info] Daemon "' + pkg.name + '" :: ' + msg);
+  },
+  error: function(msg) {
+    console.error('[' + new Date, '] [error] Daemon "' + pkg.name + '" :: ' + msg);
+  },
+  debug: function(msg) {
+    console.log('[' + new Date, '] [debug] Daemon "' + pkg.name + '" :: ' + msg);
+  },
+};
+
+
 module.exports = function(config, logger) {
   config = config || {};
-  logger = logger || {
-    info: function(msg) {
-      console.info('[' + new Date + '] [info] Daemon "' + pkg.name + '" :: ' + msg);
-    },
-    error: function(msg) {
-      console.error('[' + new Date, '] [error] Daemon "' + pkg.name + '" :: ' + msg);
-    },
-    debug: function(msg) {
-      console.log('[' + new Date, '] [debug] Daemon "' + pkg.name + '" :: ' + msg);
-    },
-  };
+  logger = logger || defaultLogger;
 
 
   // defaults
   config.address = config.address || 'localhost';
   config.port = config.port || 5984;
   config.streams = config.streams && parseInt(config.streams, 10) || 50;
-  config.changes_feed_timeout = config.changes_feed_timeout && parseInt(config.changes_feed_timeout, 10) || 60 * 1000;
+  config.timeout = config.timeout && parseInt(config.timeout, 10) || 60 * 1000;
 
-  // TODO: validate config
+  // validate config
+  assert.equal(typeof config.streams, 'number', 'streams must be a number');
+  assert.equal(parseInt(config.streams), config.streams, 'streams must be an interger');
+  assert.ok(config.streams > 0, 'streams must be a positive non-zero');
+  assert.equal(typeof config.timeout, 'number', 'timeout must be a number');
+  assert.equal(parseInt(config.timeout), config.timeout, 'timeout must be an interger');
+  assert.ok(config.timeout > 0, 'timeout must be a positive non-zero');
 
 
-  var couch = url.format({
+  var couchUrl = url.format({
     protocol: 'http',
     hostname: config.address,
     port: config.port,
     auth: config.auth && config.auth.username && config.auth.password ? [ config.auth.username, config.auth.password ].join(':') : null
   });
+  var couch = nano(couchUrl);
 
   var options = {
     feed: 'continuous',
-    changes_feed_timeout: config.changes_feed_timeout
+    timeout: config.timeout
   };
 
+
   function listen(dbname, next) {
-    logger.info('Listening on ' + couch + '/' + dbname);
+    logger.info('Listening on ' + couchUrl + '/' + dbname);
 
-    var db = nano(couch).use(dbname);
-
-    var changes = stream(db, options, logger);
-
-    changes.on('error', logger.error);
-    changes.on('data', function(data) {
-      if (data.response) {
-        logger.info(data.response);
-      }
-    });
-
-    changes.on('end', next);
+    stream(couch.use(dbname), options, logger)
+      .on('error', logger.error)
+      .on('data', function(data) {
+        if (data.response) {
+          logger.info(data.response);
+        }
+      })
+      .on('end', next);
   }
 
 
@@ -73,7 +83,7 @@ module.exports = function(config, logger) {
 
     // get list of all databases
     // TODO: listen to db changes
-    nano(couch).db.list(function(err, dbs) {
+    couch.db.list(function(err, dbs) {
       if (err) {
         logger.error('Can not get _all_dbs: ' + err.description);
 
